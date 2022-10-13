3147fa0ad8ec861d323ba47dbaf746899db324a6
Mai Mai <ntmainbm6328@gmail.com>
2022-10-13 20:45:20 +0700

    create api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const { connectToDb, db } = require("./db");
+const userRouter = require("./router/user");
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json({ extended: true }));
@@ -9,6 +10,8 @@ app.use(bodyParser.json({ extended: true }));
 app.get("/", (req, res) => {
   res.send("hello!");
 });
+
+app.use("/user", userRouter);
 
 app.listen(port, () => {
   console.log(`App is on port ${port}`);
--- router/user.js
+++ router/user.js
@@ -5,3 +5,124 @@
 
 // })
 // module.exports = userRouter;
+const express = require("express");
+const userRouter = express.Router();
+const { ObjectId } = require("mongodb");
+const { db } = require("../db");
+// here we create our Route
+userRouter.post("/", async (req, res) => {
+  // const User = {
+  //     password: req.body.password,
+  //     role:req.body.role,
+  //     username:req.body.username
+  // };
+  const User = ({ username, password, role } = req.body);
+  const result = await db.users.insertOne(User);
+
+  if (!result) {
+    res.json({
+      status: "FAILED",
+      message: "Không thêm được tài khoản",
+    });
+  } else {
+    res.json({
+      status: "SUCCESS",
+      message: "Thêm tài khoản thành công",
+      data: User,
+    });
+  }
+});
+// get the
+
+userRouter.get("/", async (req, res) => {
+  const result = await db.users.find({}).toArray();
+
+  if (!result) {
+    res.json({
+      status: "FAILED",
+      message: "Không có dữ liệu",
+    });
+  } else {
+    res.json({
+      status: "SUCCESS",
+      message: "Lấy được dữ liệu",
+      data: result,
+    });
+  }
+});
+
+//get the
+userRouter.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await db.users
+    .find({
+      _id: ObjectId(id),
+    })
+    .toArray();
+
+  if (!result) {
+    res.json({
+      status: "FAILED",
+      message: "Không có dữ liệu",
+    });
+  } else {
+    res.json({
+      status: "SUCCESS",
+      message: "Lấy được dữ liệu",
+      data: result,
+    });
+  }
+});
+
+//get the
+userRouter.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const filter = {
+    _id: new ObjectId(id),
+  };
+  const updateDoc = {
+    $set: body,
+  };
+  const result = await db.users.updateOne(filter, updateDoc);
+
+  if (!result) {
+    res.json({
+      status: "FAILED",
+      message: "Không có dữ liệu",
+    });
+  } else {
+    res.json({
+      status: "SUCCESS",
+      message: "Lấy được dữ liệu",
+      data: result,
+    });
+  }
+});
+
+userRouter.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const filter = {
+    Idchu: id,
+  };
+  const updateDoc = {
+    $set: body,
+  };
+  const result = await db.users.deleteOne(filter);
+
+  if (!result) {
+    res.json({
+      status: "FAILED",
+      message: "Không có dữ liệu",
+    });
+  } else {
+    res.json({
+      status: "SUCCESS",
+      message: "Lấy được dữ liệu",
+      data: result,
+    });
+  }
+});
+
+module.exports = userRouter;

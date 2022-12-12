7512e0d1b4db2c7be8d66326bf0558b4536b6b95
dohuykhanh <88757246+dohuykhanh@users.noreply.github.com>
2022-12-12 20:24:37 +0700

    feature task

--- db.js
+++ db.js
@@ -1,21 +1,22 @@
 const { MongoClient } = require("mongodb");
 
 const url =
-  "mongodb+srv://todo:todo123456@cluster0.dqbwvlw.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://todo:todo123456@cluster0.dqbwvlw.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 const dbName = "User_Management";
 const db = {};
 
 async function connectToDb() {
-  await client.connect();
-  console.log("Connected successfully to Database");
-  const database = client.db(dbName);
+    await client.connect();
+    console.log("Connected successfully to Database");
+    const database = client.db(dbName);
 
-  db.users = database.collection("User");
-  db.todos = database.collection("Task");
-  db.items = database.collection("Item");
-  db.groups = database.collection("Group");
-  return "done.";
+    db.users = database.collection("User");
+    db.todos = database.collection("Task");
+    db.Task = database.collection("Task");
+    db.items = database.collection("Item");
+    db.groups = database.collection("Group");
+    return "done.";
 }
 
 module.exports = { connectToDb, db };
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { connectToDb, db } = require("./db");
 const userRouter = require("./router/user");
 const uploadRouter = require("./router/upload");
 const todoRouter = require("./router/task");
+const TaskRouter = require("./router/TaskRouter");
 let itemRouter = require("./router/item");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
@@ -14,14 +15,15 @@ app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("hello!");
+    res.send("hello!");
 });
 app.use("/upload", express.static("uploads"));
 app.use("/user", userRouter, uploadRouter);
 app.use("/todo", todoRouter);
 app.use("/item", itemRouter);
+app.use("/Task", TaskRouter);
 
 app.listen(port, () => {
-  console.log(`App is on port ${port}`);
-  connectToDb();
+    console.log(`App is on port ${port}`);
+    connectToDb();
 });
